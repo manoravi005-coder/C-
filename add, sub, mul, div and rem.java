36c978919c8c8785048449571d84336a0d607304
Manobala.R <manoravi005@gmail.com>
2026-07-22 09:15:08 +0530

    Add basic arithmetic operations in Main class

---
+++ add, sub, mul, div and rem.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[]args){
+        System.out.println(3+4);
+        System.out.println(Math.abs(3-4));
+        System.out.println(3*4);
+        System.out.println(3/4);
+        System.out.println(3%4);
+        
+    }
+}
