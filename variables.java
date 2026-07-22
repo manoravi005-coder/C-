2d16f68cc52f631d36431f74b5364ee266eea32c
Manobala.R <manoravi005@gmail.com>
2026-07-22 09:29:14 +0530

    Add Main class with variable declarations and output

---
+++ variables.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[]args){
+        int no = 5;
+        float no2 = 10.23f;
+        char name = 'M';
+        boolean bool = true;
+        String text = "Jack";
+        System.out.printf("%d%n%.2f%n%c%n%b%n%s", no, no2, name, bool, text);
+        
+    }
+}
